0b77a43e38e2b580b6874ea45c0c302b03ec1f93
bob <1498833800@qq.com>
2023-11-07 16:44:38 +0800

    add router;

--- src/Pages/Layout/menuConfig.tsx
+++ src/Pages/Layout/menuConfig.tsx
@@ -2,176 +2,176 @@ import Icon from "@/Components/Icon";
 import styleScope from "./menu.module.less";
 export const menuList = [
   {
-    key: "/work-mange",
+    key: "/ozfund/work-mange",
     icon: <Icon name="h-icon-shiwuguanli" className={styleScope["icon"]} />,
     label: "Ozfund事务管理",
     children: [
       {
-        key: "/work-mange/todo",
+        key: "/ozfund/work-mange/todo",
         icon: <></>,
         label: "待办事务",
       },
       {
-        key: "/work-mange/mange-site",
+        key: "/ozfund/work-mange/mange-site",
         icon: <></>,
         label: "管理员设置",
       },
       {
-        key: "/work-mange/business-mange",
+        key: "/ozfund/work-mange/business-mange",
         icon: <></>,
         label: "TOTO业务管理",
       },
       {
-        key: "/work-mange/toto-contract",
+        key: "/ozfund/work-mange/toto-contract",
         icon: <></>,
         label: "TOTO合约设置",
       },
       {
-        key: "/work-mange/ozc-contract",
+        key: "/ozfund/work-mange/ozc-contract",
         icon: <></>,
         label: "OZC合约设置",
       },
     ],
   },
   {
-    key: "/website-operation",
+    key: "/ozfund/website-operation",
     icon: <Icon name="h-icon-wangzhanyunying" className={styleScope["icon"]} />,
     label: "Ozfund网站运营",
     children: [
       {
-        key: "/website-operation/zh-cn",
+        key: "/ozfund/website-operation/zh-cn",
         icon: <></>,
         label: "简体中文",
       },
       {
-        key: "/website-operation/zh-wt",
+        key: "/ozfund/website-operation/zh-wt",
         icon: <></>,
         label: "繁体中文",
       },
       {
-        key: "/website-operation/en",
+        key: "/ozfund/website-operation/en",
         icon: <></>,
         label: "English",
       },
       {
-        key: "/website-operation/japan",
+        key: "/ozfund/website-operation/japan",
         icon: <></>,
         label: "しろうと",
       },
     ],
   },
   {
-    key: "/email",
+    key: "/ozfund/email",
     icon: <Icon name="h-icon-dingyueyouxiang" className={styleScope["icon"]} />,
     label: "Ozfund订阅邮箱",
     children: [
       {
-        key: "/email/list",
+        key: "/ozfund/email/list",
         icon: <></>,
         label: "邮箱列表",
       },
       {
-        key: "/email/temp",
+        key: "/ozfund/email/temp",
         icon: <></>,
         label: "邮箱模板",
       },
       {
-        key: "/email/send",
+        key: "/ozfund/email/send",
         icon: <></>,
         label: "邮件定时发送",
       },
       {
-        key: "/email/record",
+        key: "/ozfund/email/record",
         icon: <></>,
         label: "邮件发送记录",
       },
     ],
   },
   {
-    key: "/assets",
+    key: "/ozfund/assets",
     icon: <Icon name="h-icon-zichantongji" className={styleScope["icon"]} />,
     label: "资产统计",
     children: [
       {
-        key: "/assets/toto",
+        key: "/ozfund/assets/toto",
         icon: <></>,
         label: "TOTO",
       },
       {
-        key: "/assets/ozc",
+        key: "/ozfund/assets/ozc",
         icon: <></>,
         label: "OZC资产统计",
       },
       {
-        key: "/assets/toto-price",
+        key: "/ozfund/assets/toto-price",
         icon: <></>,
         label: "TOTO兑换价格核算",
       },
       {
-        key: "/assets/toto_ozc",
+        key: "/ozfund/assets/toto_ozc",
         icon: <></>,
         label: "TOTO/OZC价格统计",
       },
     ],
   },
   {
-    key: "/permission",
+    key: "/ozfund/permission",
     icon: <Icon name="h-icon-quanxianguanli" className={styleScope["icon"]} />,
     label: "权限管理",
     children: [
       {
-        key: "/permission/staff-list",
+        key: "/ozfund/permission/staff-list",
         icon: <></>,
         label: "员工列表",
       },
     ],
   },
   {
-    key: "/IP",
+    key: "/ozfund/IP",
     icon: <Icon name="h-icon-IPguanli" className={styleScope["icon"]} />,
     label: "IP管理",
     children: [
       {
-        key: "/IP/list",
+        key: "/ozfund/IP/list",
         icon: <></>,
         label: "IP列表",
       },
       {
-        key: "/IP/change-record",
+        key: "/ozfund/IP/change-record",
         icon: <></>,
         label: "IP变动记录",
       },
     ],
   },
   {
-    key: "/logs",
+    key: "/ozfund/logs",
     icon: <Icon name="h-icon-rizhiguanli" className={styleScope["icon"]} />,
     label: "日志管理",
     children: [
       {
-        key: "/logs/sign",
+        key: "/ozfund/logs/sign",
         icon: <></>,
         label: "登录日志",
       },
       {
-        key: "/logs/operation",
+        key: "/ozfund/logs/operation",
         icon: <></>,
         label: "操作日志",
       },
       {
-        key: "/logs/work",
+        key: "/ozfund/logs/work",
         icon: <></>,
         label: "事务日志",
       },
     ],
   },
   {
-    key: "/business",
+    key: "/ozfund/business",
     icon: <Icon name="h-icon-yewuguanli" className={styleScope["icon"]} />,
     label: "业务管理",
   },
   {
-    key: "/personal",
+    key: "/ozfund/personal",
     icon: <Icon name="h-icon-gerenziliao" className={styleScope["icon"]} />,
     label: "个人资料",
   },
--- src/Routers/config.jsx
+++ src/Routers/config.jsx
@@ -2,6 +2,37 @@ import Login from "@/Pages/Login";
 import LayoutPage from "@/Pages/Layout";
 import Denied from "@/Pages/Denied";
 import PersonalInfo from "@/Pages/Personal";
+import Assets from "@/Pages/Assets";
+import AssetsOzc from "@/Pages/Assets/ozc";
+import AssetsToto from "@/Pages/Assets/toto";
+import AssetsTotoOzc from "@/Pages/Assets/totoOzc";
+import AssetsTotoPrice from "@/Pages/Assets/totoPrice";
+import Business from "@/Pages/Business";
+import Email from "@/Pages/Email";
+import EmailList from "@/Pages/Email/list";
+import EmailSend from "@/Pages/Email/send";
+import EmailTemp from "@/Pages/Email/temp";
+import EmailRecord from "@/Pages/Email/record";
+import IP from "@/Pages/IP";
+import IPList from "@/Pages/IP/list";
+import IPChangeRecord from "@/Pages/IP/changeRecord";
+import Logs from "@/Pages/Logs";
+import LogsOperation from "@/Pages/logs/operation";
+import LogsSign from "@/Pages/logs/sign";
+import LogsWork from "@/Pages/logs/work";
+import Permission from "@/Pages/Permission";
+import PermissionStaffList from "@/Pages/Permission/staffList";
+import WebsiteOperation from "@/Pages/WebsiteOperation";
+import WebsiteOperationEn from "@/Pages/WebsiteOperation/en";
+import WebsiteOperationJapan from "@/Pages/WebsiteOperation/japan";
+import WebsiteOperationZhCn from "@/Pages/WebsiteOperation/zh-cn";
+import WebsiteOperationZhWt from "@/Pages/WebsiteOperation/zh-wt";
+import WorkMange from "@/Pages/WorkMange";
+import WorkMangeBusinessMange from "@/Pages/WorkMange/businessMange";
+import WorkMangeMangeSite from "@/Pages/WorkMange/mangeSite";
+import WorkOzcContract from "@/Pages/WorkMange/ozcContract";
+import WorkTodo from "@/Pages/WorkMange/todo";
+import WorkTotoContract from "@/Pages/WorkMange/totoContract";
 const RouteList = [
   {
     path: "/",
@@ -20,6 +51,175 @@ const RouteList = [
     element: <LayoutPage />,
     isAuth: true,
     children: [
+      {
+        path: 'work-mange',
+        element: <WorkMange />,
+        isAuth: true,
+        children: [
+          {
+            path: 'todo',
+            element: <WorkTodo />,
+            isAuth: true
+          },
+          {
+            path: 'mange-site',
+            element: <WorkMangeMangeSite />,
+            isAuth: true
+          },
+          {
+            path: 'business-mange',
+            element: <WorkMangeBusinessMange />,
+            isAuth: true
+          },
+          {
+            path: 'toto-contract',
+            element: <WorkTotoContract />,
+            isAuth: true
+          },
+          {
+            path: 'ozc-contract',
+            element: <WorkOzcContract />,
+            isAuth: true
+          },
+        ]
+      },
+      {
+        path: 'website-operation',
+        element: <WebsiteOperation />,
+        isAuth: true,
+        children: [
+          {
+            path: "zh-cn",
+            element: <WebsiteOperationZhCn />,
+            isAuth: true,
+          },
+          {
+            path: "zh-wt",
+            element: <WebsiteOperationZhWt />,
+            isAuth: true,
+          },
+          {
+            path: "en",
+            element: <WebsiteOperationEn />,
+            isAuth: true,
+          },
+          {
+            path: "japan",
+            element: <WebsiteOperationJapan />,
+            isAuth: true,
+          },
+        ]
+      },
+      {
+        path: 'email',
+        element: <Email />,
+        isAuth: true,
+        children: [
+          {
+            path: 'list',
+            element: <EmailList />,
+            isAuth: true,
+          },
+          {
+            path: 'temp',
+            element: <EmailTemp />,
+            isAuth: true,
+          },
+          {
+            path: 'send',
+            element: <EmailSend />,
+            isAuth: true,
+          },
+          {
+            path: 'record',
+            element: <EmailRecord />,
+            isAuth: true,
+          },
+        ]
+      },
+      {
+        path: 'assets',
+        element: <Assets />,
+        isAuth: true,
+        children: [
+          {
+            path: 'toto',
+            element: <AssetsToto />,
+            isAuth: true,
+          },
+          {
+            path: 'ozc',
+            element: <AssetsOzc />,
+            isAuth: true,
+          },
+          {
+            path: 'toto-price',
+            element: <AssetsTotoPrice />,
+            isAuth: true,
+          },
+          {
+            path: 'toto_ozc',
+            element: <AssetsTotoOzc />,
+            isAuth: true,
+          },
+        ]
+      },
+      {
+        path: 'permission',
+        element: <Permission />,
+        isAuth: true,
+        children: [
+          {
+            path: 'staff-list',
+            element: <PermissionStaffList />,
+            isAuth: true,
+          },
+        ]
+      },
+      {
+        path: 'IP',
+        element: <IP />,
+        isAuth: true,
+        children: [
+          {
+            path: 'list',
+            element: <IPList />,
+            isAuth: true,
+          },
+          {
+            path: 'change-record',
+            element: <IPChangeRecord />,
+            isAuth: true,
+          },
+        ]
+      },
+      {
+        path: 'logs',
+        element: <Logs />,
+        isAuth: true,
+        children: [
+          {
+            path: 'sign',
+            element: <LogsSign />,
+            isAuth: true,
+          },
+          {
+            path: 'operation',
+            element: <LogsOperation />,
+            isAuth: true,
+          },
+          {
+            path: 'work',
+            element: <LogsWork />,
+            isAuth: true,
+          },
+        ]
+      },
+      {
+        path: 'business',
+        element: <Business />,
+        isAuth: true,
+      },
       {
         path: 'personal',
         element: <PersonalInfo />,
