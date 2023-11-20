0fb1e304d578a16cccc09c5f44ee1fd3109d67cd
bob <1498833800@qq.com>
2023-11-20 15:31:49 +0800

    fix;

--- src/Pages/WorkMange/businessMange/releaseCustody/index.tsx
+++ src/Pages/WorkMange/businessMange/releaseCustody/index.tsx
@@ -1,4 +1,58 @@
+import { ModalTitle } from "@/Components/Modal";
+import { Button, ConfigProvider, Form, InputNumber, Select } from "antd";
 const TotoReleaseCustody = () => {
-  return <>TOTo解押</>;
+  return (
+    <div className="h-full overflow-y-auto bg-white rounded-[var(--border-radius)] mt-[.15rem]">
+      <TitleComp title="解押TOTO质押挖矿" />
+      <ConfigProvider
+        theme={{
+          components: {
+            Input: {
+              paddingBlock: 6,
+            },
+            InputNumber: {
+              paddingBlock: 6,
+            },
+            Form: {
+              labelColor: "#666",
+            },
+          },
+          token: {
+            controlHeight: 36,
+            borderRadius: 2,
+          },
+        }}
+      >
+        <Form
+          layout="vertical"
+          className="grid grid-cols-2 gap-x-[.2rem] py-[.2rem] pr-[.2rem] pl-[.3rem]"
+        >
+          <Form.Item label="地址" className="mb-[.15rem]">
+            <Select placeholder="选择Token" options={[]} />
+          </Form.Item>
+          <Form.Item label="数量" className="mb-[.15rem]">
+            <InputNumber className="w-full" placeholder="输入数量" />
+          </Form.Item>
+
+          <Form.Item className="mb-0" />
+          <Form.Item className="flex justify-end mb-0">
+            <Button className="w-[.75rem] ml-[.1rem]" type="primary">
+              确认
+            </Button>
+          </Form.Item>
+        </Form>
+      </ConfigProvider>
+    </div>
+  );
+};
+const TitleComp = ({ title }) => {
+  return (
+    <ModalTitle
+      showTitleIcon
+      title={title}
+      classTitleName="py-[.2rem] ml-[.3rem]  border-b border-b-[#e6e6e6] text-[16px] text-[#333]"
+      classIconName="w-[.03rem] h-[.13rem]"
+    />
+  );
 };
 export default TotoReleaseCustody;
