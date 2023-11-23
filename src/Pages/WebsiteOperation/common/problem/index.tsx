ea35c08c43959ee0ad8788f089cc8721048a8b59
bob <1498833800@qq.com>
2023-11-23 17:56:12 +0800

    update

--- src/Pages/WebsiteOperation/common/problem/index.tsx
+++ src/Pages/WebsiteOperation/common/problem/index.tsx
@@ -8,23 +8,29 @@ import ModalScope from "@/Pages/ModalComp";
 import { useStopPropagation } from "@/Hooks/StopPropagation";
 const Problem = () => {
   let [addProblemInfoOpen, setAddProblemInfoOpen] = useState(false);
+  let [showFilterComp, setShowFilterComp] = useState(false);
   let [stop] = useStopPropagation();
+
   let moduleContent = useRef<any>();
   let moduleTitle = useRef<any>();
+
   function addProblemCb(e) {
     stop(e, () => {
       moduleContent.current = EditorOrAddProblem;
       moduleTitle.current = "新增问题分类";
-      setAddProblemInfoOpen(!addProblemInfoOpen);
+      // setAddProblemInfoOpen(!addProblemInfoOpen);
+      setShowFilterComp(!showFilterComp);
     });
   }
   function lookCb(e, crt, index) {
     stop(e, () => {
-      moduleContent.current = EditorOrAddProblem;
-      moduleTitle.current = "查看问题分类";
-      setAddProblemInfoOpen(!addProblemInfoOpen);
+      // moduleContent.current = EditorOrAddProblem;
+      // moduleTitle.current = "查看问题分类";
+      // setAddProblemInfoOpen(!addProblemInfoOpen);
+      setShowFilterComp(!showFilterComp);
     });
   }
+
   return (
     <>
       <div className="flex bg-white justify-end p-[var(--gap20)] rounded-[var(--border-radius)]">
@@ -37,14 +43,12 @@ const Problem = () => {
           新增问题分类
         </Button>
       </div>
-      <div
-        className={mergeClassName(
-          "bg-white rounded-[var(--border-radius)] mt-[var(--gap15)] pb-[var(--gap14)]"
-        )}
-      >
-        <TableProblem onLook={lookCb} />
-      </div>
-      <MoreBtn />
+      {showFilterComp ? (
+        <FilterContentModule onLook={lookCb} />
+      ) : (
+        <ContentModule onLook={lookCb} />
+      )}
+
       <ModalComp
         content={moduleContent.current}
         title={moduleTitle.current}
@@ -52,6 +56,76 @@ const Problem = () => {
         onCancel={() => setAddProblemInfoOpen(!addProblemInfoOpen)}
         onOk={(values) => setAddProblemInfoOpen(!addProblemInfoOpen)}
       />
+    </>
+  );
+};
+const FilterContentModule = (props) => {
+  let problemListFilter = [
+    {
+      id: "A",
+      title: "A类问题",
+    },
+    {
+      id: "B",
+      title: "B类问题",
+    },
+    {
+      id: "C",
+      title: "C类问题",
+    },
+  ];
+  let [stop] = useStopPropagation();
+  let [activeFilter, setActiveFilter] = useState("");
+  function filterItemCb(e, crt) {
+    stop(e, () => {
+      setActiveFilter(crt.id);
+    });
+  }
+  function lookCb() {
+    props?.onLook?.();
+  }
+  return (
+    <div className="grid grid-cols-[2rem_1fr] gap-x-[var(--gap15)] ">
+      <div className="rounded-[var(--border-radius)] w-[2rem] overflow-hidden bg-white mt-[var(--gap15)]">
+        <p className="grid place-items-center w-full h-[65px] bg-[#fafafa] rounded-[var(--border-radius)]">
+          <span className="text-[16px] text-[#333]">问题分类</span>
+        </p>
+        <ul className="w-full">
+          {problemListFilter.map((item) => (
+            <li
+              onClick={(e) => filterItemCb(e, item)}
+              key={item.id}
+              className="grid place-items-center h-[.6rem] border-b border-b-[#e9e9e9]"
+            >
+              <span
+                className={mergeClassName(
+                  "cursor-pointer",
+                  activeFilter == item.id ? "text-[var(--blue)]" : ""
+                )}
+              >
+                {item.title}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <ContentModule onLook={lookCb} />
+      </div>
+    </div>
+  );
+};
+
+const ContentModule = (props) => {
+  function lookCb() {
+    props?.onLook?.();
+  }
+  return (
+    <>
+      <div className="bg-white rounded-[var(--border-radius)] mt-[var(--gap15)] pb-[var(--gap14)]">
+        <TableProblem onLook={lookCb} />
+      </div>
+      <MoreBtn />
     </>
   );
 };
