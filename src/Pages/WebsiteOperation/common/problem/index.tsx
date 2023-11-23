6a95e6df4d5c225dc0c052adf46a017fd5dba2f3
bob <1498833800@qq.com>
2023-11-23 15:10:26 +0800

    update;

--- src/Pages/WebsiteOperation/common/problem/index.tsx
+++ src/Pages/WebsiteOperation/common/problem/index.tsx
@@ -1,4 +1,147 @@
+import { PlusOutlined } from "@ant-design/icons";
+import { Button, ConfigProvider, Form, Input } from "antd";
+import TableProblem from "./table";
+import MoreBtn from "@/Components/MoreBtn";
+import { forwardRef, useRef, useState } from "react";
+import { mergeClassName } from "@/utils/base";
+import ModalScope from "@/Pages/ModalComp";
+import { useStopPropagation } from "@/Hooks/StopPropagation";
 const Problem = () => {
-  return <>常见问题</>;
+  let [addProblemInfoOpen, setAddProblemInfoOpen] = useState(false);
+  let [stop] = useStopPropagation();
+  let moduleContent = useRef<any>();
+  let moduleTitle = useRef<any>();
+  function addProblemCb(e) {
+    stop(e, () => {
+      moduleContent.current = EditorOrAddProblem;
+      moduleTitle.current = "新增问题分类";
+      setAddProblemInfoOpen(!addProblemInfoOpen);
+    });
+  }
+  function lookCb(e, crt, index) {
+    stop(e, () => {
+      moduleContent.current = EditorOrAddProblem;
+      moduleTitle.current = "查看问题分类";
+      setAddProblemInfoOpen(!addProblemInfoOpen);
+    });
+  }
+  return (
+    <>
+      <div className="flex bg-white justify-end p-[var(--gap20)] rounded-[var(--border-radius)]">
+        <Button
+          onClick={addProblemCb}
+          type="primary"
+          className="py-[6px] h-[.36rem]"
+          icon={<PlusOutlined />}
+        >
+          新增问题分类
+        </Button>
+      </div>
+      <div
+        className={mergeClassName(
+          "bg-white rounded-[var(--border-radius)] mt-[var(--gap15)] pb-[var(--gap14)]"
+        )}
+      >
+        <TableProblem onLook={lookCb} />
+      </div>
+      <MoreBtn />
+      <ModalComp
+        content={moduleContent.current}
+        title={moduleTitle.current}
+        modalOpen={addProblemInfoOpen}
+        onCancel={() => setAddProblemInfoOpen(!addProblemInfoOpen)}
+        onOk={(values) => setAddProblemInfoOpen(!addProblemInfoOpen)}
+      />
+    </>
+  );
 };
+const EditorOrAddProblem = (props) => {
+  let [stop] = useStopPropagation();
+  let [form] = Form.useForm();
+  let [formInitVal] = useState({
+    problemType: "",
+  });
+  function submitCb(values) {
+    console.log("values: ", values);
+    props?.onOk(values);
+  }
+  function cancelCb(e) {
+    stop(e, () => {
+      props?.onCancel?.(false);
+    });
+  }
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          borderRadius: 2,
+        },
+      }}
+    >
+      <Form
+        layout="vertical"
+        onFinish={submitCb}
+        initialValues={formInitVal}
+        form={form}
+      >
+        <Form.Item
+          className="mb-[var(--gap30)] mx-[var(--gap30)] mt-[var(--gap20)]"
+          label={<span className="text-[var(--border-color)]">问题分类</span>}
+          name="problemType"
+        >
+          <Input size="large" placeholder="请输入问题分类" />
+        </Form.Item>
+
+        <Form.Item className="mb-0">
+          <ModalFooter onCancel={(e) => cancelCb(e)} />
+        </Form.Item>
+      </Form>
+    </ConfigProvider>
+  );
+};
+const ModalFooter = (props) => {
+  return (
+    <div className="flex justify-end py-[.28rem] pr-[.28rem] border-t border-t-[#e6e6e6]">
+      {props.only ? (
+        <>
+          <Button type="primary" onClick={(e) => props?.onSubmit(e)}>
+            确定
+          </Button>
+        </>
+      ) : (
+        <>
+          <Button onClick={(e) => props?.onCancel(e)} className="mr-[.1rem]">
+            取消
+          </Button>
+          <Button type="primary" htmlType="submit">
+            确定
+          </Button>
+        </>
+      )}
+    </div>
+  );
+};
+const ModalComp = forwardRef((props: any, ref: any) => {
+  function cancelCb(value) {
+    props?.onCancel?.(value);
+  }
+  function okCb(values) {
+    props?.onOk?.(values);
+  }
+  let CompName = props.content;
+  return props.modalOpen ? (
+    <ModalScope
+      onCancel={cancelCb}
+      showFooter={false}
+      modalOpen={true}
+      body={{
+        paddingInline: "0",
+        paddingBlock: "0",
+      }}
+      title={props.title}
+    >
+      <CompName onCancel={cancelCb} onOk={okCb} />
+    </ModalScope>
+  ) : null;
+});
 export default Problem;
---
+++ src/Pages/WebsiteOperation/common/problem/table.tsx
@@ -0,0 +1,100 @@
+import TableComp from "@/Components/Table";
+import type { ColumnsType } from "@/Components/Table";
+import { ConfigProvider, Switch, Typography } from "antd";
+import styleScope from "./index.module.less";
+import { useState } from "react";
+import { DeleteOutlined, EditOutlined, EyeFilled } from "@ant-design/icons";
+import { useStopPropagation } from "@/Hooks/StopPropagation";
+const TableProcess = (props) => {
+  const columns: ColumnsType = [
+    {
+      title: "序号",
+      dataIndex: "frezzTime",
+      render: (_, record, index) => index + 1,
+    },
+    {
+      title: "问题分类",
+      dataIndex: "address",
+    },
+    {
+      title: "置顶",
+      dataIndex: "num",
+      render: (_, record, index) => {
+        return <Switch checked />;
+      },
+    },
+    {
+      title: "操作",
+      dataIndex: "operation",
+      width: 280,
+      align: "left",
+      render: (_, record, index) => {
+        const editable = isEditing(record);
+        return (
+          <div className="flex gap-[var(--gap10)]">
+            <Typography.Link disabled={editable}>
+              <div
+                onClick={(e) => props?.onLook?.(e, record, index)}
+                className="flex items-center justify-center h-[.3rem] w-[.76rem] bg-[var(--blue1)] rounded-[4px] text-[var(--blue)]"
+              >
+                <EyeFilled className="mr-[8px]" />
+                <span>查看</span>
+              </div>
+            </Typography.Link>
+            <Typography.Link disabled={editable}>
+              <div className="flex items-center justify-center h-[.3rem] w-[.76rem] bg-[var(--green1)] rounded-[4px] text-[var(--green2)]">
+                <EditOutlined className="mr-[8px]" />
+                <span>编辑</span>
+              </div>
+            </Typography.Link>
+            <Typography.Link disabled={editable}>
+              <div
+                onClick={(e) => deleteCb(e, record)}
+                className="flex items-center justify-center h-[.3rem] w-[.76rem] bg-[#eeeff0] rounded-[4px] text-[#53585E]"
+              >
+                <DeleteOutlined className="mr-[8px]" />
+                <span>移除</span>
+              </div>
+            </Typography.Link>
+          </div>
+        );
+      },
+    },
+  ];
+  const [dataList, setDataList] = useState<any>([
+    {
+      key: 1,
+      frezzTime: new Date(),
+      address: "djahoaic4234kahdiuahdajag",
+      num: 439487,
+      notes: "Ozfund投注挖矿：sifjsidijjisd-Ozfund投注挖矿：Aioeowie",
+      staffId: "xiaowu",
+    },
+  ]);
+  let [stop] = useStopPropagation();
+  let [editingKey, setEditingKey] = useState("");
+  let isEditing = (record) => record.key === editingKey;
+  function deleteCb(e, crt) {
+    stop(e, () => {
+      props?.onDelete?.(crt);
+    });
+  }
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          borderRadius: 2,
+          controlHeight: 36,
+        },
+      }}
+    >
+      <TableComp
+        className={styleScope["tabel_reset"]}
+        dataSource={dataList}
+        columns={columns}
+      />
+    </ConfigProvider>
+  );
+};
+
+export default TableProcess;
