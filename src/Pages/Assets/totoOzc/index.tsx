69f8dfcdf203faacd8198365b2b32ca209148649
bob <1498833800@qq.com>
2023-11-26 14:24:37 +0800

    fix

--- src/Pages/Assets/totoOzc/index.tsx
+++ src/Pages/Assets/totoOzc/index.tsx
@@ -1,4 +1,48 @@
-const TotoOzc = () => {
-  return <>TotoOzc</>;
+import TableAllocation from "./table";
+import { useEffect, useRef, useState } from "react";
+import MoreBtn from "@/Components/MoreBtn";
+const TotoOzc = (props) => {
+  let [chartsData] = useState([
+    {
+      date: "2023/07/01",
+      produce: 100,
+      destroy: 80,
+      inflow: 20,
+    },
+    {
+      date: "2023/07/02",
+      produce: 100,
+      destroy: 100,
+      inflow: 100,
+    },
+    {
+      date: "2023/07/03",
+      produce: 100,
+      destroy: 100,
+      inflow: 100,
+    },
+    {
+      date: "2023/07/04",
+      produce: 100,
+      destroy: 100,
+      inflow: 100,
+    },
+  ]);
+  let tableRefs = useRef<any>();
+  let [tableHeight, setTableHeight] = useState<number>();
+  useEffect(() => {
+    setTimeout(() => {
+      let { height } = tableRefs.current.getBoundingClientRect();
+      setTableHeight(height);
+    }, 100);
+  }, []);
+  return (
+    <div className="h-full overflow-y-auto">
+      <div className="mt-[var(--gap15)] pb-[var(--gap14)] bg-white rounded-[0_0_var(--border-radius)_var(--border-radius)]">
+        <TableAllocation />
+      </div>
+      <MoreBtn />
+    </div>
+  );
 };
 export default TotoOzc;
---
+++ src/Pages/Assets/totoOzc/table.tsx
@@ -0,0 +1,59 @@
+import TableComp from "@/Components/Table";
+import type { ColumnsType } from "@/Components/Table";
+import { ConfigProvider } from "antd";
+import { useState } from "react";
+const TableAllocation = (props) => {
+  const columns: ColumnsType = [
+    {
+      title: "时间",
+      dataIndex: "frezzTime",
+      render: (_, record, index) => index + 1,
+    },
+    {
+      title: "TOTO流通数量",
+      dataIndex: "address",
+    },
+    {
+      title: "OZC流通数量",
+      dataIndex: "num",
+    },
+    {
+      title: "TOTO/OZC",
+      dataIndex: "num",
+    },
+  ];
+  const [dataList, setDataList] = useState<any>([
+    {
+      key: 1,
+      frezzTime: new Date(),
+      address: "djahoaic4234kahdiuahdajag",
+      num: 439487,
+      notes: "Ozfund投注挖矿：sifjsidijjisd-Ozfund投注挖矿：Aioeowie",
+      staffId: "xiaowu",
+    },
+  ]);
+  return (
+    <ConfigProvider
+      theme={{
+        components: {
+          Table: {
+            headerColor: "#333",
+            colorText: "#666",
+          },
+        },
+        token: {
+          borderRadius: 2,
+          controlHeight: 36,
+        },
+      }}
+    >
+      <TableComp
+        className="_reset-tabel"
+        dataSource={dataList}
+        columns={columns}
+      />
+    </ConfigProvider>
+  );
+};
+
+export default TableAllocation;
