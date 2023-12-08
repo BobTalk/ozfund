8ed67d59164fd02e192e914f86f26ea37ce1c898
bob <1498833800@qq.com>
2023-12-08 16:38:43 +0800

    管理员设置 接口暂缓

--- src/Pages/WorkMange/mangeSite/index.tsx
+++ src/Pages/WorkMange/mangeSite/index.tsx
@@ -3,72 +3,92 @@ import ModalComp from "@/Pages/ModalComp";
 import closeIcon from "@/assets/images/close.svg";
 import plusIcon from "@/assets/images/plus.svg";
 import { Button, Form, Input } from "antd";
-import { useState } from "react";
+import { useRef, useState } from "react";
 const MangeSite = () => {
   let [mangeOpen, setMangeOpen] = useState(false);
   let [isAddManage, setIsAddManage] = useState(false);
+  let crtManageInfo = useRef<any>();
   let [addmanage, setAddmanage] = useState({
     name: "",
   });
+  let [manageList, setmanageList] = useState([
+    {
+      id: 1,
+      name: "超级管理员A",
+      address: "",
+    },
+    {
+      id: 2,
+      name: "超级管理员B",
+      address: "",
+    },
+    {
+      id: 3,
+      name: "管理员A",
+      address: "",
+    },
+    {
+      id: 4,
+      name: "管理员B",
+      address: "",
+    },
+    {
+      id: 5,
+      name: "管理员C",
+      address: "",
+    },
+  ]);
   let [stop] = useStopPropagation();
   function onFinish() {}
   function manageCb() {
     setMangeOpen(!mangeOpen);
   }
-  function deleteCb(e) {
-    stop(e, manageCb);
+  function deleteCb(crt, crtIdx) {
+    crtManageInfo.current = { ...crt, flag: "删除", index: crtIdx };
+    setMangeOpen(!mangeOpen);
     setIsAddManage(false);
   }
-  function addCb(e) {
-    stop(e, manageCb);
+  function addCb(crt, crtIdx) {
+    crtManageInfo.current = { ...crt, flag: "添加", index: crtIdx };
+    setMangeOpen(!mangeOpen);
     setIsAddManage(true);
+  }
+  function operationCb(e, item, index) {
+    stop(e, () => {
+      (item.address ? deleteCb : addCb)(item, index);
+    });
   }
   return (
     <>
       <ul className="h-full bg-[var(--white)] rounded-[var(--border-radius)] py-[var(--gap20)]">
-        <li className="grid items-center ml-[.4rem] py-[var(--gap20)] border-b border-b-[var(--border-color)] border-dashed pr-[var(--gap20)] grid-cols-[20%_1fr_max-content] gap-[var(--gap15)] text-[14px] text-[#333]">
-          <span>Bob.Li -超级管理员A</span>
-          <span>bdjkhafheuiwfhkhiureofufherhiuehfi23fcdushod</span>
-          <span onClick={deleteCb} className="flex items-center cursor-pointer">
-            <img width={15} height={15} src={closeIcon} alt="关闭" />
-            <span className="ml-[.1rem]">删除</span>
-          </span>
-        </li>
-        <li className="grid items-center ml-[.4rem] py-[var(--gap20)] border-b border-b-[var(--border-color)] border-dashed pr-[var(--gap20)] grid-cols-[20%_1fr_max-content] gap-[var(--gap15)] text-[14px] text-[#333]">
-          <span>Bob.Li -超级管理员B</span>
-          <span>bdjkhafheuiwfhkhiureofufherhiuehfi23fcdushod</span>
-          <span className="flex items-center cursor-pointer">
-            <img width={15} height={15} src={closeIcon} alt="关闭" />
-            <span className="ml-[.1rem]">删除</span>
-          </span>
-        </li>
-        <li className="grid items-center ml-[.4rem] py-[var(--gap20)] border-b border-b-[var(--border-color)] border-dashed pr-[var(--gap20)] grid-cols-[20%_1fr_max-content] gap-[var(--gap15)] text-[14px] text-[#333]">
-          <span>管理员A</span>
-          <span>暂无</span>
-          <span onClick={addCb} className="flex items-center cursor-pointer">
-            <img width={15} height={15} src={plusIcon} alt="添加" />
-            <span className="ml-[.1rem] text-[var(--blue)]">添加</span>
-          </span>
-        </li>
-        <li className="grid items-center ml-[.4rem] py-[var(--gap20)] border-b border-b-[var(--border-color)] border-dashed pr-[var(--gap20)] grid-cols-[20%_1fr_max-content] gap-[var(--gap15)] text-[14px] text-[#333]">
-          <span>Bob.Li -管理员B</span>
-          <span>bdjkhafheuiwfhkhiureofufherhiuehfi23fcdushod</span>
-          <span className="flex items-center cursor-pointer">
-            <img width={15} height={15} src={closeIcon} alt="关闭" />
-            <span className="ml-[.1rem]">删除</span>
-          </span>
-        </li>
-        <li className="grid items-center ml-[.4rem] py-[var(--gap20)] border-b border-b-[var(--border-color)] border-dashed pr-[var(--gap20)] grid-cols-[20%_1fr_max-content] gap-[var(--gap15)] text-[14px] text-[#333]">
-          <span>Bob.Li -超级管理员C</span>
-          <span>bdjkhafheuiwfhkhiureofufherhiuehfi23fcdushod</span>
-          <span onClick={deleteCb} className="flex items-center cursor-pointer">
-            <img width={15} height={15} src={closeIcon} alt="关闭" />
-            <span className="ml-[.1rem]">删除</span>
-          </span>
-        </li>
+        {manageList.map((item, index) => (
+          <li
+            key={item.id}
+            className="grid items-center ml-[.4rem] py-[var(--gap20)] border-b border-b-[var(--border-color)] border-dashed pr-[var(--gap20)] grid-cols-[20%_1fr_max-content] gap-[var(--gap15)] text-[14px] text-[#333]"
+          >
+            <span>{item.name}</span>
+            <span>{item.address || "暂无"}</span>
+            <span
+              onClick={(e) => operationCb(e, item, index)}
+              className="flex items-center cursor-pointer"
+            >
+              <img
+                width={15}
+                height={15}
+                src={item.address ? closeIcon : plusIcon}
+                alt=""
+              />
+              {item.address ? (
+                <span className="ml-[.1rem]">删除</span>
+              ) : (
+                <span className="ml-[.1rem] text-[var(--blue)]">添加</span>
+              )}
+            </span>
+          </li>
+        ))}
       </ul>
       <ModalComp
-        title={(isAddManage ? "添加" : "移除") + "管理员"}
+        title={`${crtManageInfo.current?.flag}${crtManageInfo.current?.name}`}
         showFooter={!isAddManage}
         footer={{
           paddingInline: ".28rem",
@@ -82,7 +102,7 @@ const MangeSite = () => {
           <Form layout="vertical" onFinish={onFinish} initialValues={addmanage}>
             <Form.Item
               label={
-                <span className="text-[var(--border-color)]">管理员A</span>
+                <span className="text-[var(--border-color)]">{crtManageInfo.current?.name}</span>
               }
               className="px-[var(--gap30)] my-[var(--gap30)] "
               name="name"
