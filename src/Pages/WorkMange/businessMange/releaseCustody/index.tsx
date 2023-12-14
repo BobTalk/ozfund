fcaf639305006bf54964fe6398095bdbdf2c14af
bob <1498833800@qq.com>
2023-12-14 18:06:25 +0800

    update

--- src/Pages/WorkMange/businessMange/releaseCustody/index.tsx
+++ src/Pages/WorkMange/businessMange/releaseCustody/index.tsx
@@ -1,6 +1,22 @@
 import { ModalTitle } from "@/Components/Modal";
-import { Button, ConfigProvider, Form, InputNumber, Select } from "antd";
+import { DischargeStakeInterface } from "@/api";
+import {
+  Button,
+  ConfigProvider,
+  Form,
+  Input,
+  InputNumber,
+  Select,
+  message,
+} from "antd";
 const TotoReleaseCustody = () => {
+  let [form] = Form.useForm();
+  async function submitInfo({ address }) {
+    let { status, message: tipInfo } = await DischargeStakeInterface({
+      address,
+    });
+    message[status ? "success" : "error"](tipInfo);
+  }
   return (
     <div className="h-full overflow-y-auto bg-white rounded-[var(--border-radius)] mt-[var(--gap15)]">
       <TitleComp title="解押TOTO质押挖矿" />
@@ -24,19 +40,45 @@ const TotoReleaseCustody = () => {
         }}
       >
         <Form
+          form={form}
+          onFinish={submitInfo}
           layout="vertical"
-          className="grid grid-cols-2 gap-x-[var(--gap20)] py-[var(--gap20)] pr-[var(--gap20)] pl-[var(--gap30)]"
+          className="grid clear_required grid-cols-2 gap-x-[var(--gap20)] py-[var(--gap20)] pr-[var(--gap20)] pl-[var(--gap30)]"
         >
-          <Form.Item label="地址" className="mb-[var(--gap15)]">
-            <Select placeholder="选择Token" options={[]} />
+          <Form.Item
+            rules={[
+              {
+                required: true,
+                message: "",
+              },
+            ]}
+            name="address"
+            label="地址"
+            className="mb-[var(--gap15)]"
+          >
+            <Input placeholder="输入地址" />
           </Form.Item>
-          <Form.Item label="数量" className="mb-[var(--gap15)]">
+          <Form.Item
+            rules={[
+              {
+                required: true,
+                message: "",
+              },
+            ]}
+            name="num"
+            label="数量"
+            className="mb-[var(--gap15)]"
+          >
             <InputNumber className="w-full" placeholder="输入数量" />
           </Form.Item>
 
           <Form.Item className="mb-0" />
           <Form.Item className="flex justify-end mb-0">
-            <Button className="w-[.75rem] ml-[.1rem]" type="primary">
+            <Button
+              className="w-[.75rem] ml-[.1rem]"
+              htmlType="submit"
+              type="primary"
+            >
               确认
             </Button>
           </Form.Item>
